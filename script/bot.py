b70d13117ff9e7280eab510a21138e306ccb2037
Kim Suyun <103942316+VoidAsMad@users.noreply.github.com>
2023-02-05 01:26:46 +0900

    Update bot.py

--- script/bot.py
+++ script/bot.py
@@ -1,5 +1,5 @@
 from config import PointConfig
-from discord import Object
+from discord import Interaction, Object
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix=PointConfig.prefix, intents=PointConfig.intents())
@@ -9,6 +9,16 @@ bot = commands.Bot(command_prefix=PointConfig.prefix, intents=PointConfig.intent
 async def on_ready():
     bot.tree.copy_global_to(guild=Object(id=PointConfig.guild_id))
     await bot.tree.sync(guild=Object(id=PointConfig.guild_id))
+
+
+@bot.command(name="핑")
+async def content_ping(ctx: commands.Context) -> None:
+    return await ctx.send("퐁")
+
+
+@bot.tree.command(name="핑", description="퐁을 대답합니다.")
+async def slash_ping(inter: Interaction) -> None:
+    return await inter.response.send_message("퐁")
 
 
 bot.run(PointConfig.token)
