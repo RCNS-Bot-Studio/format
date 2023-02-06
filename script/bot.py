06aa8d7793bb1f222c58a4bb7254508b07742799
Kim Suyun <103942316+VoidAsMad@users.noreply.github.com>
2023-02-06 22:50:34 +0900

    Update bot.py

--- script/bot.py
+++ script/bot.py
@@ -11,12 +11,12 @@ async def on_ready():
     await bot.tree.sync(guild=Object(id=Config.guild_id))
 
 
-@bot.command(name="핑")
+@bot.command(name="핑") # 접두사 (!) 커맨드
 async def content_ping(ctx: commands.Context) -> None:
     return await ctx.send("퐁")
 
 
-@bot.tree.command(name="핑", description="퐁을 대답합니다.")
+@bot.tree.command(name="핑", description="퐁을 대답합니다.") # 빗금 (/) 커맨드
 async def slash_ping(inter: Interaction) -> None:
     return await inter.response.send_message("퐁")
 
